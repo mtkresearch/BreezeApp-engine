746166cbb0f6b4c08dbee7e67ea20e8fafd748b7
muxi <louis19980927@gmail.com>
2025-12-23 09:28:51 +0800

    chore: bump version to 1.9.0 (21)

--- android/breeze-app-engine/build.gradle.kts
+++ android/breeze-app-engine/build.gradle.kts
@@ -42,8 +42,8 @@ android {
         applicationId = "com.mtkresearch.breezeapp.engine"
         minSdk = 34
         targetSdk = 35
-        versionCode = 20
-        versionName = "1.8.6"
+        versionCode = 21
+        versionName = "1.9.0"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
